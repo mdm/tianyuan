4e75d832e5094d98ed81c91cef1f7f223dfaba87
mdm <marcmigge@gmx.net>
2016-07-22 18:59:34 +0200

    First steps for hand crafted sgf parser

--- tianyuan/gametree.py
+++ tianyuan/gametree.py
@@ -1,20 +1,29 @@
-class MoveNode:
+class GameTreeNode:
     def __init__(self):
-        pass
-    def add_move()
+        self.properties = {}
+    def property(self, identifier, value):
+        self.properties[identifier] = value
         
 class GameTree:
     def __init__(self):
-        pass
-    def add_node(self, node, parent):
-        pass
+        self.root = None
+        self.children = {}
+    def add_node(self, node, parent = None):
+        if parent:
+            self.children.setdefault(parent, []).append(node)
+        else:
+            self.root = node
+    def remove_node(self, node):
+        del(self.children[node])
+    def get_children(self, node):
+        return self.children[node]
 
 class GameTreeBuilder:
     def __init__(self, game_tree):
-        pass
+        self.parent_stack = []
     def add_variation(self):
         pass
-    def add_move(self, coordinates):
+    def add_node(self, node):
         pass
         
 
